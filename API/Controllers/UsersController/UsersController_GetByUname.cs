b870a3c027eec5074eea42421e7dbc0da43eb19d
Zakharii-Husar <zakhariihusar@gmail.com>
2024-04-07 22:52:15 -0500

    Added user by username controller

---
+++ API/Controllers/UsersController/UsersController_GetByUname.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using API.Models;
+using API.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace API.Controllers.UsersController
+{
+    public partial class UsersController
+    {
+
+        [HttpGet("{userName}")]
+        public async Task<ActionResult<UserDTO>> GetUserDetails(string userName)
+        {
+            var user = await userManager.FindByNameAsync(userName);
+            if (user == null) return NotFound();
+            return usersService.ConvertUserToDTO(user);
+        }
+    }
+}
+
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd6494e1e2e6a38b1c7d7f5f0ec500e161ca850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9c88800ec69fd41d24c8a4c5983d60f55f7ee5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
