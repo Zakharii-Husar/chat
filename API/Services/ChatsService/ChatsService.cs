e5397e9786746e024902dcc3b3dd511b02bca2cc
Zakharii-Husar <zakhariihusar@gmail.com>
2024-04-17 10:39:23 -0500

    Merged AuthService

--- API/Services/ChatsService/ChatsService.cs
+++ API/Services/ChatsService/ChatsService.cs
@@ -11,25 +11,29 @@ namespace API.Services.ChatsService
 {
     public interface IChatsService
     {
-        public Task<bool> SendMsgAsync(int chatId, SendMessageModel model, string currentUserId);
-        public Task<bool> CheckMembershipByChatIdAsync(int chatId, string userId);
-        public Task<bool> CheckMembershipByMsgIdAsync(int messageId, string userId);
         public Task<bool> MarkChatAsReadAsync(int chatId, AppUser currentUser);
-        public Task<bool> RmChatMemberAsync(int chatId, string candidatUname, AppUser currentUser);
-        public Task<bool> AddChatMemberAsync(int chatId, string candidatUname, AppUser currentUser);
         public Task<bool> RenameChatAsync(int chatId, string newName, AppUser currentUser);
         public Task<int?> GetPrivateChatIdAsync(string uname1, string uname2);
         public Task<int?> CreatePrivateChatAsync(string uname1, string uname2);
         public Task<int?> CreateGroupChatAsync(NewChatModel chatModel, AppUser currentUser);
         public Task<List<MessageDTO>> GetChatsOverviewAsync(string userId, int itemsToSkip, int itemsToTake);
-
         public Task<ChatDTO?> GetChatDTOAsync(string userId, int chatId, int itemsToSkip, int itemsToTake);
-        public Task<List<string>> GetMembersIdsAsync(int chatId);
 
+        //MEMBERSHIP SERVICE
+        public Task<bool> RmChatMemberAsync(int chatId, string candidatUname, AppUser currentUser);
+        public Task<bool> AddChatMemberAsync(int chatId, string candidatUname, AppUser currentUser);
+        public Task<List<string>> GetMembersIdsAsync(int chatId);
         public Task<bool> CheckRoleAsync(int chatId, string userId);
+        public Task<bool> CheckMembershipByChatIdAsync(int chatId, string userId);
+        public Task<bool> CheckMembershipByMsgIdAsync(int messageId, string userId);
+
+        //MESSAGE
+        public Task<bool> SendMsgAsync(int chatId, SendMessageModel model, string currentUserId);
         public Task<bool> AddLikeAsync(int messageId, string currentUserId);
         public Task<bool> RmLikeAsync(int messageId, string currentUserId);
         public Task<bool> MarkMsgAsDelAsync(int messageId, string currentUserId);
+
+        //WS SERVICE
         public Task WSBroadcastMessageAsync(Message newMessage);
     }
     public partial class ChatsService(
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d80bdd514ea42ec2f33b150ea58fd6b893b0a37d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19192a8cd7f1420dacfa7eee6a03eeb12edef714")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
