359df9df46fb5e01ca9328bf5f4b62e934b16420
Zakharii-Husar <zakhariihusar@gmail.com>
2025-01-26 15:35:21 -0600

    fix homepage

--- client/src/redux/thunks/APIEndpoints.ts
+++ client/src/redux/thunks/APIEndpoints.ts
@@ -6,9 +6,9 @@ const getBasePath = () => {
 
 // Modify API_URL to include the base path in production
 export const API_URL = process.env.REACT_APP_API_URL || 
-  `${window.location.origin}${getBasePath()}/chat-api`;
+  `${window.location.origin}${getBasePath()}/projects/chat/chat-api`;
 export const WS_URL = process.env.REACT_APP_WS_URL || 
-  `${window.location.origin}${getBasePath()}/Hub`;
+  `${window.location.origin}${getBasePath()}/projects/chat/Hub`;
 
   console.log(API_URL);
 //AUTHORIZATION
