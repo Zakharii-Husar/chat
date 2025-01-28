76e701563802b1612adc37597e5c1f4d99a9c6c5
Zakharii-Husar <zakhariihusar@gmail.com>
2025-01-27 19:24:49 -0600

    Fixed race conditions by using Abort controller in my thunks

--- client/src/redux/thunks/getAllChatsThunk.ts
+++ client/src/redux/thunks/getAllChatsThunk.ts
@@ -3,23 +3,27 @@ import { GET_ALL_CHATS } from "./APIEndpoints";
 import { appendChats, setHasMore } from "../slices/chatsOverviewSlice";
 import { RootState } from "../store";
 
+let abortController = new AbortController();
+
 const getAllChatsThunk = createAsyncThunk(
   "chats/getAllChats",
   async (_, { getState, dispatch }) => {
+    abortController.abort();
+    abortController = new AbortController();
+
     const state = getState() as RootState;
     const itemsToTake = 10;
     const itemsToSkip = state.chats.chats.length;
     const link = GET_ALL_CHATS(itemsToSkip, itemsToTake);
     try {
-      const response = await fetch(link,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        }
-      );
+      const response = await fetch(link, {
+        signal: abortController.signal,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
       if (response.ok) {
         const data = await response.json();
@@ -27,6 +31,9 @@ const getAllChatsThunk = createAsyncThunk(
         if(data.length < itemsToTake) dispatch(setHasMore());
       }
     } catch (error) {
+      if (error instanceof Error && error.name === 'AbortError') {
+        return;
+      }
       console.log(error);
     }
   }
--- client/src/redux/thunks/getChatByIdThunk.ts
+++ client/src/redux/thunks/getChatByIdThunk.ts
@@ -3,28 +3,38 @@ import { GET_CHAT_BY_ID } from "./APIEndpoints";
 import type { RootState } from "../store";
 import { appendMsgs } from "../slices/currentChatSlice";
 
+let abortController = new AbortController();
+
 const getChatByIdThunk = createAsyncThunk(
   "currentChat/getChatById",
   async (chatId: number, { getState, dispatch }) => {
+    // Cancel any pending request
+    abortController.abort();
+    abortController = new AbortController();
+
     const state = getState() as RootState;
     const itemsToSkip = state.currentChat.messages.length;
-    const link = GET_CHAT_BY_ID(chatId, itemsToSkip)
+    const link = GET_CHAT_BY_ID(chatId, itemsToSkip);
+    
     try {
-      const response = await fetch(link,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          credentials: "include",
-        }
-      );
-
+      const response = await fetch(link, {
+        signal: abortController.signal,
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      
       if (response.ok) {
         const data = await response.json();
         dispatch(appendMsgs(data));
       }
     } catch (error) {
+      if (error instanceof Error && error.name === 'AbortError') {
+        // Handle abort
+        return;
+      }
       console.log(error);
     } 
   }
