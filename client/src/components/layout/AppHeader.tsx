b2774fbd945fbebc7b711bcc67f749539901a482
Zakharii-Husar <zakhariihusar@gmail.com>
2025-01-29 18:35:52 -0600

    Modified header to show the number of unread messages

--- client/src/components/layout/AppHeader.tsx
+++ client/src/components/layout/AppHeader.tsx
@@ -10,6 +10,14 @@ import "./AppHeader.scss";
 export const AppHeader: React.FC = () => {
   useCheckAuth();
   const currentUser = useAppSelector((state) => state.loggedInUser);
+  const chats = useAppSelector((state) => state.chats.chats);
+  
+  const unreadCount = chats.filter(chat => 
+    chat.senderId !== currentUser.id && 
+    chat.seenBy.length === 0
+  ).length;
+
+  const displayCount = unreadCount > 9 ? "9+" : unreadCount;
 
   return (
     <header className="app-header">
@@ -34,7 +42,14 @@ export const AppHeader: React.FC = () => {
         <div className="app-header__right">
           {currentUser.id && (
             <Link to="/chats" className="app-header__link">
-              <IoIosMail size={32} className="app-header__icon" />
+              <div className="notification-wrapper">
+                <IoIosMail size={32} className="app-header__icon" />
+                {unreadCount > 0 && (
+                  <span className="notification-badge">
+                    {displayCount}
+                  </span>
+                )}
+              </div>
             </Link>
           )}
         </div>
