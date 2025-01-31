9eeb91a1074078a0a193582d7266b65e2ef22498
Zakharii-Husar <zakhariihusar@gmail.com>
2025-01-30 20:48:43 -0600

    Changed dotnet configurations

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+395c583e86945badc17c5807cfaeea2fb27c0e67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+575519decc391794c3f13dee32d3c8924b836782")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
